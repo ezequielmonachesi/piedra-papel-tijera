04c8de68325fd62c4a1d003aa17fe62afabec089
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-03-20 02:49:46 -0300

    terminado

--- js/script.js
+++ js/script.js
@@ -1,74 +1,49 @@
-/* 
-piedra 1
-papel 2
-tijera 3 */
-/* 
+let valorIngresado;
 
-piedra1 G tijera2 P papel3
-
-tijera G papel P piedra
-
-papel G piedra P tijera
-
-piedra = 1
-tijera = 2
-papel = 3*/
-
-let valorIngresado = parseInt(prompt('Elija con número su opción: 1-Piedra, 2-Papel, 3-Tijera'))
-
-function piedra(valor){
-  let tijera = 2
-  let random = Math.floor(Math.random() * 3) + 1
-  if (random == valor) {
-    document.write(`Tu elegiste Piedra y yo elegí Piedra ||| Empatamos :/`);
-  } else if(random == tijera) {
-    document.write(`Tu elegiste Piedra y yo elegí Tijera ||| Ganaste D=`);
+do {
+  valorIngresado = parseInt(
+    prompt("Elija con número su opción: 1-Piedra, 2-Papel, 3-Tijera")
+  );
+  if (valorIngresado == 1) {
+    piedra(valorIngresado);
+  } else if (valorIngresado == 2) {
+    papel(valorIngresado);
+  } else if (valorIngresado == 3){
+    tijera(valorIngresado);
   }else{
-    document.write(`Tu elegiste Piedra y yo elegí Papel ||| Perdiste =D`);
+    alert('Ingresa un numero valido')
   }
-}
-
-
-function tijera(valor){
-  let random = Math.floor(Math.random() * 3) + 1
-  if (random == valor) {
-    document.write(`Empate`);
-  } else if(random == 2) {
-    document.write(`Ganaste`);
-  }else{
-    document.write(`Perdiste`);
+  function piedra(valor) {
+    let tijera = 2;
+    let random = Math.floor(Math.random() * 3) + 1;
+    if (random === valor) {
+      alert(`Tu elegiste Piedra y yo elegí Piedra ||| Empatamos! :/`);
+    } else if (random === tijera) {
+      alert(`Tu elegiste Piedra y yo elegí Tijera ||| Me Ganaste! )=`);
+    } else {
+      alert(`Tu elegiste Piedra y yo elegí Papel ||| Te Gané! =D`);
+    }
   }
-}
-
-if (valorIngresado == 1){
-  piedra(valorIngresado)
-}else if (valorIngresado == 2){
-  papel(valorIngresado)
-}else{
-  tijera(valorIngresado)
-}
-
-
-/* 
-
-if (piedra == "piedra") {
-  document.write(`Ganaste`);
-} else {
-  document.write(`Perdiste`);
-}
-if (tijera == "tijera") {
-  document.write(`Ganaste`);
-} else {
-  document.write(`Perdiste`);
-}
-if (papel == "papel") {
-  document.write(`Ganaste`);
-} else {
-  document.write(`Perdiste`);
-} */
-
-/* 
-for (let i = 0; i < 5; i++) {
-    random = Math.floor(Math.random() * 3) + 1
-    document.write(`${random}`)
-} */
+  function tijera(valor) {
+    let papel = 2;
+    let random = Math.floor(Math.random() * 3) + 1;
+    if (random === valor) {
+      alert(`Tu elegiste Tijera y yo elegí Tijera ||| Empatamos! :/`);
+    } else if (random === papel) {
+      alert(`Tu elegiste Tijera y yo elegí Papel ||| Me Ganaste! )=`);
+    } else {
+      alert(`Tu elegiste Tijera y yo elegí Piedra ||| Te Gané! =D`);
+    }
+  }
+  function papel(valor) {
+    let piedra = 2;
+    let random = Math.floor(Math.random() * 3) + 1;
+    if (random === valor) {
+      alert(`Tu elegiste Papel y yo elegí Papel ||| Empatamos! :/`);
+    } else if (random === piedra) {
+      alert(`Tu elegiste Papel y yo elegí Piedra ||| Me Ganaste! )=`);
+    } else {
+      alert(`Tu elegiste Papel y yo elegí Tijera ||| Te Gané! =D`);
+    }
+  }
+} while (confirm("Quieres jugar de nuevo?"));
